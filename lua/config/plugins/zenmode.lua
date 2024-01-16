ae268ae630a0829d4b17f0029749da3822de4361
miahaidarc <mateus.haidar@icloud.com>
2024-01-16 02:35:25 -0300

    configured cmp and luasnip but no lsp

---
+++ lua/config/plugins/zenmode.lua
@@ -0,0 +1,14 @@
+return -- Lua
+{
+  "folke/zen-mode.nvim",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+        kitty = {
+      enabled = false,
+      font = "+4", -- font size increment
+    },
+
+  }
+}
